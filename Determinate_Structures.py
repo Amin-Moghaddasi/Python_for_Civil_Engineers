4e7c77bbb95217e537975fb11fd5efa493595d2f
Amin_Moghaddasi <105602151+Amin-Moghaddasi@users.noreply.github.com>
2023-10-10 09:15:14 +0000

    Update Determinate_Structures.py

--- Determinate_Structures.py
+++ Determinate_Structures.py
@@ -2,15 +2,54 @@ from sympy import symbols, integrate
 
 
 class Simple_Beam:
-    def simple_beam_dist_load(P_x, l):
+    @staticmethod
+    def simple_beam_dist_load_Ay(P_x, l, x_0, x_1):
         x = symbols('x')
         expr_1 = x * P_x
-        Ay = integrate(P_x, (x, 0, l)) - (integrate(expr_1, (x, 0, l))) / l
-        By = integrate(expr_1, (x, 0, l)) / l
-        return Ay, By
+        Ay = integrate(P_x, (x, x_0, x_1)) - (integrate(expr_1, (x, x_0, x_1))) / l
 
-    def simple_const_beam(l, a, P):
+        return Ay
+
+    @staticmethod
+    def simple_beam_dist_load_By(P_x, l, x_0, x_1):
+        x = symbols('x')
+        expr_1 = x * P_x
+        By = integrate(expr_1, (x, x_0, x_1)) / l
+        return  By
+
+    @staticmethod
+    def simple_const_beam_Ay(l, a, P):
         b = l - a
         Ay = (P * b) / l
+        return Ay
+    @staticmethod
+    def simple_const_beam_By(l, a, P):
+        b = l - a
         By = (P * a) / l
+        return By
+
+
+    @staticmethod
+    def combo(loads):
+        dist_list =[]
+        const_list =[]
+        print(len(loads))
+        for i in range (len(loads)):
+                if loads[i][0] == 'Dist':
+                    dist_list.append(loads[i])
+                elif loads[i][0] == 'Const':
+                    const_list.append(loads[i])
+                else:
+                    print("Error!")
+        Ay = 0
+        By = 0
+        print(f"Const list : {const_list}")
+        print(f"Dist list : {dist_list}")
+        for k in range(len(dist_list)):
+            Ay += Simple_Beam.simple_beam_dist_load_Ay(P_x =dist_list[k][1],l= dist_list[k][2],x_0=dist_list[k][3],x_1=dist_list[k][4])
+            By += Simple_Beam.simple_beam_dist_load_By(P_x=dist_list[k][1], l=dist_list[k][2], x_0=dist_list[k][3],
+                                                x_1=dist_list[k][4])
+        for g in range(len(const_list)):
+            Ay += Simple_Beam.simple_const_beam_Ay(l=const_list[g][1],a=const_list[g][2],P=const_list[g][3])
+            By += Simple_Beam.simple_const_beam_By(l=const_list[g][1],a=const_list[g][2],P=const_list[g][3])
         return Ay, By
