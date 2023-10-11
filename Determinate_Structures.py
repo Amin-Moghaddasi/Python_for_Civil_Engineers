c6fbf4915901aef6c9daa70a02b4e7b32952a1f3
Amin_Moghaddasi <105602151+Amin-Moghaddasi@users.noreply.github.com>
2023-10-11 08:23:38 +0000

    Update Determinate_Structures.py

--- Determinate_Structures.py
+++ Determinate_Structures.py
@@ -53,3 +53,47 @@ class Simple_Beam:
             Ay += Simple_Beam.simple_const_beam_Ay(l=const_list[g][1],a=const_list[g][2],P=const_list[g][3])
             By += Simple_Beam.simple_const_beam_By(l=const_list[g][1],a=const_list[g][2],P=const_list[g][3])
         return Ay, By
+class Cantilever_Beam:
+    @staticmethod
+    def cantilever_const_beam_Ay(P):
+        Ay = P
+        return Ay
+    @staticmethod
+    def cantilver_const_beam_M_A(P, a):
+        M_A = P * a
+        return M_A
+    @staticmethod
+    def cantilever_dist_beam_A_y(P_x, x_0, x_1):
+        x = symbols('x')
+        A_y = integrate(P_x,(x,x_0,x_1))
+        return A_y
+    @staticmethod
+    def cantilever_dist_beam_M_A(P_x, x_0, x_1):
+        x = symbols('x')
+        expr = x * P_x
+        M_A = integrate(expr, (x, x_0, x_1))
+        return M_A
+
+    @staticmethod
+    def combo(loads):
+        dist_list = []
+        const_list = []
+        print(len(loads))
+        for i in range(len(loads)):
+            if loads[i][0] == 'Dist':
+                dist_list.append(loads[i])
+            elif loads[i][0] == 'Const':
+                const_list.append(loads[i])
+            else:
+                print("Error!")
+        Ay = 0
+        M_A = 0
+        print(f"Const list : {const_list}")
+        print(f"Dist list : {dist_list}")
+        for k in range(len(dist_list)):
+            Ay += Cantilever_Beam.cantilever_dist_beam_A_y(P_x=dist_list[k][1], x_0=dist_list[k][2], x_1=dist_list[k][3])
+            M_A += Cantilever_Beam.cantilever_dist_beam_M_A(P_x=dist_list[k][1],x_0=dist_list[k][2],x_1=dist_list[k][3])
+        for g in range(len(const_list)):
+            Ay += Cantilever_Beam.cantilever_const_beam_Ay(P=const_list[g][1])
+            M_A += Cantilever_Beam.cantilver_const_beam_M_A(P=const_list[g][1],a=const_list[g][2])
+        return Ay, M_A
